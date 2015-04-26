3bed56f59a9d618cb344d9f553c16001b61f9022
snigdhak40 <snigdhak40@gmail.com>
2015-04-26 00:09:12 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,14 +16,13 @@
 
 ##########################################################################################################
 
-
 # Clean up workspace
 rm(list=ls())
 
 # 1. Merge the training and the test sets to create one data set.
 
 #set working directory to the location where the UCI HAR Dataset was unzipped
-setwd('/Users/heather/Documents/UCI HAR Dataset/');
+setwd("C:\\specdata\\UCI HAR Dataset\\");
 
 # Read in the data from files
 features     = read.table('./features.txt',header=FALSE); #imports features.txt
@@ -113,4 +112,4 @@ tidyData    = aggregate(finalDataNoActivityType[,names(finalDataNoActivityType) 
 tidyData    = merge(tidyData,activityType,by='activityId',all.x=TRUE);
 
 # Export the tidyData set 
-write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t');
+write.table(tidyData, 'C:\\specdata\\tidyData.txt',row.names=FALSE,sep='\t');
